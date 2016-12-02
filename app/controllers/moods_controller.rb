84640108d8b0bbbf3626290e4ca703010789754b
EstebanLFalcon <estebanlf92@gmail.com>
2016-12-01 16:14:34 -0800

    to_json super

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,10 +1,10 @@
 class MoodsController < ApplicationController
   def index
     moods = Mood.all
-    moods = moods.select('emotional_state')
-    moods.each do |h|
-      h.delete('id')
-    end
+    # moods = moods.select('emotional_state')
+    # moods.each do |h|
+    #   h.delete('id')
+    # end
     render json: moods.to_json, status: 200
   end
 end
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,7 @@
 class PlacesController < ApplicationController
+  def to_json(options = {})
+    super(options.merge({ except: [:id] }))
+  end
   def index
     places = Place.all
     render json: places.select('name').to_json, status: 200
